1530613d5577705e53598fe6d8cdb826eb6c9f7b
Bipasa Gyawali <bipasagyawali@metropolia.fi>
2025-09-04 21:42:11 +0300

    now

---
+++ ZanderSizeCheck.py
@@ -0,0 +1,7 @@
+zander_length = int(input("Enter the length of the zander in centimeters: "))
+
+if zander_length < 42:
+    difference = 42 - zander_length
+    print(f"Release the fish back into the lake! It is {difference} cm below the size limit.")
+else:
+    print("Good catch! The zander meets the size limit.")
