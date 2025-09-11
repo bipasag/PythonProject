69b9e017e556fed37d1838a56ba7ca82c7dbc35e
Bipasa Gyawali <bipasagyawali@metropolia.fi>
2025-09-11 22:12:48 +0300

    Program 4 – guessing game updated.

---
+++ GuessingGame.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess the number (1-10): "))
+    if guess < number:
+        print("Too low")
+    elif guess > number:
+        print("Too high")
+    else:
+        print("Correct! You guessed it.")
+        break
